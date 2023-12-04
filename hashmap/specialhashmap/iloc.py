faf790320def452b9e679198a4dedd9d0be5f1db
злата <118795314+dzhoshua@users.noreply.github.com>
2023-12-04 22:21:12 +0800

    add ValueError

--- hashmap/specialhashmap/iloc.py
+++ hashmap/specialhashmap/iloc.py
@@ -5,5 +5,7 @@ class Iloc(dict):
         self._dict = _dict
 
     def __getitem__(self, index):
+        if not isinstance(index, int) or index > len(self._dict) or index < 0:
+            raise ValueError("Invalid index")
         sorted_keys = sorted(self._dict.keys())
         return self._dict[sorted_keys[index]] 
